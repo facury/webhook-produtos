8400f27c2591cd2ea3cdbbbb6e38a95b3c745261
facury <facury@yahoo.com.br>
2026-03-24 14:18:01 -0300

    Update produtos.js

--- api/produtos.js
+++ api/produtos.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
 
     const url = `https://nikimba.com.br/wp-json/wc/v3/products?search=${encodeURIComponent(cleanQuery)}&per_page=20&orderby=date&order=desc&status=publish&stock_status=instock`;
 
-    const auth = Buffer.from("SUA_CONSUMER_KEY:SUA_CONSUMER_SECRET").toString("base64");
+    const auth = Buffer.from("ck_0bda750a7b71cf6c7bf8c243b9c889e250cbb5b1:cs_1d38210914534ee7d7729da8a2aae473301f78d2").toString("base64");
 
     const response = await fetch(url, {
       headers: {
